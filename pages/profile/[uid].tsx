fd613cfbff6ad28ad4443f9755bf31070c48ccc6
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-03-08 15:57:44 +0900

    프로필 페이지 패딩 수정

--- pages/profile/[uid].tsx
+++ pages/profile/[uid].tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { useUserStore } from "../../stores/userStore";
 
 const UserProfile = () => {
-  const { user } = useUserStore();
+  const { user, isGuest } = useUserStore();
 
   return (
-    <div className="flex w-full flex-col p-PageLR">
-      <h3>안녕하세요 {user?.displayName ? user.displayName : "노루"} 님!</h3>
+    <div className="flex w-full flex-col p-PageLR pt-[10vh] text-Primary">
+      <h4>
+        {isGuest && "안녕하세요 게스트님 :)"}
+        {!isGuest && `${user?.email} 님 :)`}
+      </h4>
+      <p className="text-lg">해당 페이지는 준비 중 입니다</p>
     </div>
   );
 };
